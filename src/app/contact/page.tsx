bb6384ffed836dce3d3226a399c51847d3226096
adagency12 <adagenccy4u@gmail.com>
2026-01-27 23:31:41 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,7 +18,7 @@ export default function ContactPage() {
         message: ''
     });
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         
         try {
